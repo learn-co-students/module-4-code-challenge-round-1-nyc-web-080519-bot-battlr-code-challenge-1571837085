56a9d2631d91f103f63835a49e52ea2defc3e3db
jonlouey <louey.jonathan@gmail.com>
2019-10-23 11:06:01 -0400

    Jonathan Louey Module 4 Challenge

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -2,7 +2,6 @@ import React from "react";
 import YourBotArmy from "./YourBotArmy"
 import BotCollection from "./BotCollection"
 
-
 class BotsPage extends React.Component {
   state = {
     myCollection: []
